21c226c84b22716196570c56b6e82b5d3cb56364
niels <niels@iconica.nl>
2023-12-12 12:09:19 +0100

    created repository

--- lib/flutter_connectivity.dart
+++
@@ -1,7 +0,0 @@
-library flutter_connectivity;
-
-export 'src/services/connectivity_service.dart';
-
-export 'src/models/connectivity_config.dart';
-export 'src/models/connectivity_handler.dart';
-export 'src/models/connectivity_checker.dart';
